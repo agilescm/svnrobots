75e149c3bb4891e661e4f56e006913692bb00d26
unknown <yunshandi@YUNSHANDI-NB.tencent.com>
2013-01-09 13:51:47 +0800

    optimize help method.

--- SVNMerger/merger.py
+++ SVNMerger/merger.py
@@ -263,11 +263,14 @@ merger: automatic merge one svn branch to another,
 	if no conflicts, it will commit changes to subversion,
 	if meet conflicts, it will create a patch.
 
-	usage: merger.py sourceurl desturl 
+	usage: merger.py source dest username passwd
 
 	options:
-	sourceurl -- the subversion url to merge from
-	desturl -- the destination subversion url merge to
+	-s, --source	--- subversion source url to merge from
+	-d, --dest		--- subversion dest url to merge to
+	-u, --username	--- username for authenticate subversion
+	-p, --passwd	--- passwd for authenticate subversion
+	-h, --help		--- get help
 			''')
 
 	def validateSVNURL(self):
